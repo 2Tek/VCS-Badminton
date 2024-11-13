7a1b0de3be09c2ac80b161849f48e267e0c9fa58
Toan Nguyen <nguyenvantoan263@gmail.com>
2024-11-13 21:12:59 +0800

    update api and remove comments in auth.py

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, jsonify, abort
-from sqlalchemy import exc
+from sqlalchemy import exc, desc
 import json
 from flask_cors import CORS
+from datetime import datetime
+from pytz import timezone
 
-from .database.models import db_drop_all, setup_db, Court, CourtRegistration
+from .database.models import setup_db, Court, CourtRegistration
 from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-# Database reset for testing
-db_drop_all()
 
 # COURT ROUTES
 # Create a new court
@@ -20,6 +20,7 @@ db_drop_all()
 def create_court(payload):
     try:
         body = request.get_json()
+        print("Request Body: ",body)
         name = body.get('name')
         court_no = body.get('court_no')
         date = body.get('date')
@@ -31,14 +32,14 @@ def create_court(payload):
             abort(400, 'All fields are required.')
 
         # get the last court by id 
-        court = Court.query.order_by(Court.id.desc()).first()
-        if not court:
-            id = 0
-        else:
-            id = court.id
+        # Determine the new court's ID
+        court = Court.query.order_by(desc(Court.id)).first()
+        print("Court: ",court)
+        new_id = (court.id + 1) if court else 1
+        print("New ID: ",new_id)
 
         court = Court(
-            id = id + 1,
+            id = new_id,
             name=name,
             court_no=court_no,
             date=date,
@@ -48,9 +49,23 @@ def create_court(payload):
         )
         court.insert()
 
+        #get the court by id to confirm data has been updated
+        court = Court.query.get(new_id)
+        # Convert the court object to a dictionary for JSON serialization
+        court_data = {
+            'id': court.id,
+            'name': court.name,
+            'court_no': court.court_no,
+            'date': court.date,
+            'time': court.time,
+            'max_players': court.max_players,
+            'level': court.level
+        }
+
+
         return jsonify({
             'success': True,
-            'court': court
+            'court': court_data
         })
     except Exception as e:
         print(f"Create court exception: {e}")
@@ -62,10 +77,23 @@ def create_court(payload):
 def get_courts(payload):
     try:
         courts = Court.query.order_by(Court.id).all()
+        # Convert courts to JSON serializable format
+        formatted_courts = [
+            {
+                'id': court.id,
+                'name': court.name,
+                'court_no': court.court_no,
+                'date': court.date,
+                'time': court.time,
+                'max_players': court.max_players,
+                'level': court.level
+            }
+            for court in courts
+        ]
 
         return jsonify({
             'success': True,
-            'courts': courts
+            'courts': formatted_courts
         })
     except Exception as e:
         print(f"Get courts exception: {e}")
@@ -91,9 +119,22 @@ def update_court(payload, id):
         
         court.update()
 
+        #get the court by id to confirm data has been updated
+        court = Court.query.get(id)
+        # Convert the court object to a dictionary for JSON serialization
+        court_data = {
+            'id': court.id,
+            'name': court.name,
+            'court_no': court.court_no,
+            'date': court.date,
+            'time': court.time,
+            'max_players': court.max_players,
+            'level': court.level
+        }
+
         return jsonify({
             'success': True,
-            'court': court
+            'court': court_data
         })
     except Exception as e:
         print(f"Update court exception: {e}")
@@ -131,20 +172,21 @@ def delete_court(payload, id):
 def create_court_registration(payload):
     try:
         body = request.get_json()
+        print("Request Body: ",body)
         court_id = body.get('court_id')
         name = body.get('name')
         player_unique_id = body.get('player_unique_id')
-        role = body.get('role')
+        predefine_role = body.get('role')
 
         if not court_id or not name or not player_unique_id:
             abort(400, 'All fields are required.')
         
-        # get the last registration by id
-        court_registration = CourtRegistration.query.order_by(CourtRegistration.id.desc()).first()
-        if not court_registration:
-            id = 0
-        else:
-            id = court_registration.id
+        # get the last court by id 
+        # Determine the new court's ID
+        reg = CourtRegistration.query.order_by(desc(CourtRegistration.id)).first()
+        print("reg: ",reg)
+        new_id = (reg.id + 1) if reg else 1
+        print("New ID: ",new_id)
 
         # get current time in SGT time zone to string
         reg_time = datetime.now(timezone('Asia/Singapore')).strftime('%Y-%m-%d %H:%M:%S')
@@ -165,13 +207,15 @@ def create_court_registration(payload):
                     abort(400, 'Player already registered for this court.')
         
         # check if the court is full for registration to be added to waitlist
-        if len(registrations) >= max_players:
+        if predefine_role:
+            role = predefine_role
+        elif len(registrations) >= max_players:
             role = 'Waitlist'
         else:
-            role = 'Player'
+            role = 'Player'       
 
         court_registration = CourtRegistration(
-            id = id + 1,
+            id = new_id,
             court_id=court_id,
             name=name,
             player_unique_id=player_unique_id,
@@ -180,9 +224,21 @@ def create_court_registration(payload):
         )
         court_registration.insert()
 
+        #get court registration by id to confirm data has been updated
+        court_registration = CourtRegistration.query.get(new_id)
+        #format court registration to JSON serializable format
+        court_registration = {
+            'id': court_registration.id,
+            'court_id': court_registration.court_id,
+            'name': court_registration.name,
+            'player_unique_id': court_registration.player_unique_id,
+            'role': court_registration.role,
+            'reg_date_time': court_registration.reg_date_time
+        }
+
         return jsonify({
             'success': True,
-            'court_registration': court_registration.long()
+            'court_registration': court_registration
         })
     except Exception as e:
         print(f"Create court registration exception: {e}")
@@ -194,16 +250,29 @@ def create_court_registration(payload):
 def get_court_registrations(payload, court_id):
     try:
         registrations = CourtRegistration.query.filter_by(court_id=court_id).all()
+        #format registrations to JSON serializable format
+        formatted_registrations = [
+            {
+                'id': registration.id,
+                'court_id': registration.court_id,
+                'name': registration.name,
+                'player_unique_id': registration.player_unique_id,
+                'role': registration.role,
+                'reg_date_time': registration.reg_date_time
+            }
+            for registration in registrations
+        ]
 
         return jsonify({
             'success': True,
-            'registrations': registrations
+            'registrations': formatted_registrations
         })
     except Exception as e:
         print(f"Get court registrations exception: {e}")
         abort(422)
 
 # Update a court registration
+# There is some room to update this route to allow for updating the role of a player
 @app.route('/court-registrations/<string:player_unique_id>', methods=["PATCH"])
 @requires_auth('patch:court-registrations')
 def update_court_registration(payload, player_unique_id):
@@ -216,12 +285,22 @@ def update_court_registration(payload, player_unique_id):
         
         # get current time in SGT time zone to string
         reg_time = datetime.now(timezone('Asia/Singapore')).strftime('%Y-%m-%d %H:%M:%S')
-
         registration.name = body.get('name', registration.name)
-        registration.role = body.get('role', registration.role)
         registration.reg_date_time = reg_time
         
         registration.update()
+
+        #get court registration by id to confirm data has been updated
+        registration = CourtRegistration.query.get(player_unique_id)
+        #format court registration to JSON serializable format
+        registration = {
+            'id': registration.id,
+            'court_id': registration.court_id,
+            'name': registration.name,
+            'player_unique_id': registration.player_unique_id,
+            'role': registration.role,
+            'reg_date_time': registration.reg_date_time
+        }
 
         return jsonify({
             'success': True,
@@ -232,38 +311,56 @@ def update_court_registration(payload, player_unique_id):
         abort(422)
 
 # Delete a court registration
-@app.route('/court-registrations/<string:player_unique_id>', methods=["DELETE"])
+@app.route('/court-registrations', methods=["DELETE"])
 @requires_auth('delete:court-registrations')
-def delete_court_registration(payload, player_unique_id):
+def delete_court_registration(payload):
     try:
-        registration = CourtRegistration.query.get(player_unique_id)
-        if not registration:
-            abort(404)
+        body = request.get_json()
+        id = body.get('id')
+        player_unique_id = body.get('player_unique_id')
+
+        #get all registrations by player_unique_id
+        registrations = CourtRegistration.query.filter_by(player_unique_id=player_unique_id).all()
+
+        #loop through all registrations and delete them if name and court_id match
+        for registration in registrations:
+            if registration.id == id and registration.player_unique_id == player_unique_id:
+                registration.delete()
 
         registration.delete()
 
+        #change the role of the next player in the waitlist to player if a player is deleted and there is space
+        if registration.role == 'Player':
+            court = Court.query.get(registration.court_id)
+            if not court:
+                abort(404, 'Court not found.')
+            else:
+                max_players = court.max_players
+            registrations = CourtRegistration.query.filter_by(court_id=registration.court_id).all()
+            for registration in registrations:
+                if registration.role == 'Waitlist' and len(registrations) < max_players:
+                    registration.role = 'Player'
+                    registration.update()
+                    break
+
+        #get all registration of the court by court_id to confirm data has been updated
+        registrations = CourtRegistration.query.filter_by(court_id=registration.court_id).all()
+        #format registrations to JSON serializable format
+        formatted_registrations = [
+            {
+                'id': registration.id,
+                'court_id': registration.court_id,
+                'name': registration.name,
+                'player_unique_id': registration.player_unique_id,
+                'role': registration.role,
+                'reg_date_time': registration.reg_date_time
+            }
+            for registration in registrations
+        ]
+
         return jsonify({
             'success': True,
-            'delete': id
-        })
-    except Exception as e:
-        print(f"Delete court registration exception: {e}")
-        abort(422)
-
-#Delete speicific court registration by court_id and name
-@app.route('/court-registrations/<int:court_id>/<string:name>', methods=["DELETE"])
-@requires_auth('delete:court-registrations')
-def delete_court_registration_by_court_id_name(payload, court_id, name):
-    try:
-        registration = CourtRegistration.query.filter_by(court_id=court_id, name=name).first()
-        if not registration:
-            abort(404)
-
-        registration.delete()
-
-        return jsonify({
-            'success': True,
-            'delete': id
+            'updated': formatted_registrations
         })
     except Exception as e:
         print(f"Delete court registration exception: {e}")
