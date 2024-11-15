c2a7cfcbe7022d4e70b3f31c190705e9dc4c171e
Toan Nguyen <nguyenvantoan263@gmail.com>
2024-11-15 19:14:19 +0800

    working side bar menu

--- frontend/src/app/pages/court-registrations/court-registrations.page.ts
+++ frontend/src/app/pages/court-registrations/court-registrations.page.ts
@@ -12,6 +12,7 @@ export class CourtRegistrationsPage implements OnInit {
   registrationList: { [courtId: number]: CourtRegistration[] } = {};
   newCourt: Partial<Court> = {};
   canAddCourt = false;
+  isSidebarOpen = false;
 
   constructor(
     private courtService: CourtRegistrationService,
@@ -52,6 +53,10 @@ export class CourtRegistrationsPage implements OnInit {
     this.canAddCourt = true;// this.authService.can('post:court'); // Check if the user has permission to add a court
   }
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+  
   submitCourtForm() {
     if (this.newCourt.id && Number.isInteger(this.newCourt.id)) {
       // Update existing court (PATCH)
