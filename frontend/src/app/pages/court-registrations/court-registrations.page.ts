172459b09da20f2ad82dd1904363a4885ad3c3da
Toan Nguyen <nguyenvantoan263@gmail.com>
2024-11-16 14:59:57 +0800

    clear form after delete successed

--- frontend/src/app/pages/court-registrations/court-registrations.page.ts
+++ frontend/src/app/pages/court-registrations/court-registrations.page.ts
@@ -93,15 +93,13 @@ export class CourtRegistrationsPage implements OnInit {
       console.error('Player ID not found for registration:', name);
       return;
     }
-    console.log('Player ID:', playerUniqueId);
-    console.log('Being deleted ID:', beingDeletedReg);
-    console.log('Can add court:', this.canAddCourt);
     if (this.canAddCourt || playerUniqueId == beingDeletedReg.player_unique_id) {
         // get registration id from registrationList base on name, change name and reg.name to lowercase for comparison
         const _id = this.registrationList[courtId].find(reg => reg.name.toLowerCase() === name.toLowerCase()).id;
         this.courtService.removeRegistration(_id).subscribe(res => {
           if (res.success) {
             this.fetchRegistrations(); // Reload the registrations after deleting
+            this.newRegistration[courtId] = ''; // Clear the input box for this court
           }
         });
     }
