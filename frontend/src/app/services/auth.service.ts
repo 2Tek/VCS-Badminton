92c6b0156d4c76673a7df36f4b0df572f76df82f
Toan Nguyen <nguyenvantoan263@gmail.com>
2024-11-18 01:10:02 +0800

    Wait for Angular to finish routing before parsing the URL fragment

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -33,16 +33,16 @@ export class AuthService {
 
   // invoked in app.component on load
   check_token_fragment() {
-    // parse the fragment
-    const fragment = window.location.hash.substr(1).split('&')[0].split('=');
-    // check if the fragment includes the access token
-    if ( fragment[0] === 'access_token' ) {
-      // add the access token to the jwt
-      this.token = fragment[1];
-      // save jwts to localstore
-      this.set_jwt();
-    }
+    // Wait for Angular to finish routing before parsing the URL fragment
+    setTimeout(() => {
+      const fragment = window.location.hash.substr(1).split('&')[0].split('=');
+      if (fragment[0] === 'access_token') {
+        this.token = fragment[1];
+        this.set_jwt();
+      }
+    }, 0);
   }
+  
 
   set_jwt() {
     localStorage.setItem(JWTS_LOCAL_KEY, this.token);
