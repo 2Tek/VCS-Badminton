491a815ae42934ae6ad8884625c246a4099277bd
nvtoan263 <nguyenvantoan263@gmail.com>
2024-11-19 01:04:11 +0800

    using fallback

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
   { path: 'court-registrations', loadChildren: './pages/court-registrations/court-registrations.module#CourtRegistrationsPageModule' },
-
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Fallback to avoid 404 errors
 ];
 @NgModule({
   imports: [
